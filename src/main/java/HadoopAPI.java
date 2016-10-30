93a0feb520f1ea275ed2d9d58e6c75740ee5b8a5
Adrian Vazquez <adrian.vazquez@myobserver.com>
2016-10-30 19:56:31 +0100

    Added writeFile and deleteFile methods to the HadoopAPI class

--- src/main/java/HadoopAPI.java
+++ src/main/java/HadoopAPI.java
@@ -1,10 +1,12 @@
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.Tool;
 
+import java.io.PrintWriter;
 import java.io.IOException;
 
 /**
@@ -12,9 +14,21 @@ import java.io.IOException;
  */
 public class HadoopAPI extends Configured implements Tool {
     private static final String DIR_PATH = "/kschool/";
+    private static final String FILE_PATH = "/kschool/test.txt";
 
-    public void createDir(FileSystem fs) throws IOException{
+    public void createDir(FileSystem fs) throws IOException {
         fs.mkdirs(new Path(DIR_PATH));
+    }
+
+    public void writeFile(FileSystem fs) throws IOException {
+        FSDataOutputStream stream = fs.create(new Path(FILE_PATH), true);
+        PrintWriter pw = new PrintWriter(stream);
+        pw.println("Trying to write in HDFS from Java");
+        pw.close();
+    }
+
+    public void deleteFile(FileSystem fs) throws IOException {
+        fs.delete(new Path(FILE_PATH), false);
     }
 
     public int run(String[] args) throws Exception {
@@ -23,7 +37,8 @@ public class HadoopAPI extends Configured implements Tool {
 
         FileSystem fs = FileSystem.get(conf);
 
-        createDir(fs);
+        this.createDir(fs);
+        this.writeFile(fs);
 
         return 0;
     }
